a6b36ab7f2d4b8fa493560192f93201298cafac8
PuruMohite <107110840+PuruMohite@users.noreply.github.com>
2026-05-03 18:17:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function makeid(l) {
   // write your code here
+  let charArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]; //length: 62
+
+  let ans = "";
+  for(let i=0; i<l; i++){
+    let char =charArr[Math.floor(Math.random() * 62)];
+    ans += char; 
+  }
+  return ans;
+
 }
 
 // Do not change the code below.
